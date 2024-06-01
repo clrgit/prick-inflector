d376e7e332f2031290122d7c810f8be700fcb807
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2024-06-01 04:30:29 +0200

    Initial setup

---
+++ lib/prick_inflector.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "prick_inflector/version"
+
+module PrickInflector
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/prick_inflector_spec.rb
@@ -0,0 +1,7 @@
+describe PrickInflector do
+  it 'has a version number' do
+    expect(PrickInflector::VERSION).not_to be_nil
+  end
+
+  it 'does something useful'
+end
